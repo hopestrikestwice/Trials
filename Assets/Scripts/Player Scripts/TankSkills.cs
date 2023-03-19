cb5caf56535ed80b2b044b6dcd25c17bb58458b9
Sarah Pethani <sarahpethani@gmail.com>
2023-03-18 22:41:27 -0400

    changed Armourer to use Invoke instead of animation events for ending skill logic

--- Assets/Scripts/Player Scripts/TankSkills.cs
+++ Assets/Scripts/Player Scripts/TankSkills.cs
@@ -15,12 +15,21 @@ public class TankSkills : MonoBehaviourPunCallbacks, IPlayerSkills
     private PlayerUI playerUI;
 
     private Animator animator;
+    private PlayerActionCore actionCoreScript;
 
     #region Shield Variables
     [SerializeField]
     private GameObject shieldSmallParticles;
     [SerializeField]
     private GameObject shieldLargeParticles;
+    #endregion
+
+    #region Animation variables
+    // Used to tell how long the secondary/ultimate skills take
+    [SerializeField]
+    private AnimationClip secondarySkillClip;
+    [SerializeField]
+    private AnimationClip ultimateClip;
     #endregion
 
     #endregion
@@ -48,6 +57,20 @@ public class TankSkills : MonoBehaviourPunCallbacks, IPlayerSkills
             shieldSmallParticles.GetComponent<ParticleSystem>().enableEmission = false;
             shieldLargeParticles.GetComponent<ParticleSystem>().enableEmission = false;
         }
+
+        actionCoreScript = GetComponent<PlayerActionCore>();
+        if (!actionCoreScript)
+        {
+            Debug.LogError("TankSkills is Missing PlayerActionCore.cs");
+        }
+        if (!secondarySkillClip)
+        {
+            Debug.LogError("TankSkills is Missing Secondary Skill Animation Clip");
+        }
+        if (!ultimateClip)
+        {
+            Debug.LogError("TankSkills is Missing Ultimate Animation Clip");
+        }
     }
 
     #endregion
@@ -58,12 +81,17 @@ public class TankSkills : MonoBehaviourPunCallbacks, IPlayerSkills
     {
         Debug.Log("Shield button pressed.");
         animator.SetBool("isSecondarySkilling", true);
+
+        Debug.Log(secondarySkillClip.length);
+        actionCoreScript.Invoke("FinishSecondarySkillLogic", secondarySkillClip.length);
     }
 
     public void ActivateUltimate()
     {
         Debug.Log("Large shield button pressed.");
         animator.SetBool("isUltimating", true);
+
+        actionCoreScript.Invoke("FinishUltimateLogic", ultimateClip.length);
     }
     #endregion
 
